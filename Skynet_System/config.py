440f9d15bb4447227b0b2eb98fb64aee7e0dfecb
tustustushar <71062376+tustustushar@users.noreply.github.com>
2021-10-18 01:17:33 +0530

    Create config.py

---
+++ Skynet_System/config.py
@@ -0,0 +1,19 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+que = {}
+admins = {}
+
+    API_ID_KEY = "draj is op"
+    API_HASH_KEY = "lol dal isee "  
+    STRING_SESSION = "ssyour"
+    Skynet = ""
+    ENFORCERS = ""
+    Skynet_logs = ""
+    Skynet_approved_logs = ""
+    GBAN_MSG_LOGS = ""
