51bd088ffba7a9b03057ff6c48d782c6d95e1a1a
tustustushar <71062376+tustustushar@users.noreply.github.com>
2021-10-18 14:40:54 +0530

    Update __init__.py

--- Skynet_System/__init__.py
+++ Skynet_System/__init__.py
@@ -45,7 +45,7 @@ else:
     STRING_SESSION = Config.STRING_SESSION
     MONGO_DB_URL = Config.MONGO_DB_URL
     with open(os.path.join(os.getcwd(), "Skynet_System/elevated_users.json"), "r") as f:
-        data = json.load(f)
+        data = json.load
     Skynet = data["Skynet"]
     ENFORCERS = data["ENFORCERS"]
     INSPECTORS = data["INSPECTORS"]
